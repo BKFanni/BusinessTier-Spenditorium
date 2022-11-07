9b5538067a147d8e1a9660696978e553cfe79910
BKFanni <94913929+BKFanni@users.noreply.github.com>
2022-11-07 11:02:53 +0100

    Add files via upload

---
+++ BusinessTier/src/main/java/com/businesstier/BusinessTierApplication.java
@@ -0,0 +1,13 @@
+package com.businesstier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessTierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessTierApplication.class, args);
+    }
+
+}
---
+++ BusinessTier/src/test/java/com/businesstier/BusinessTierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.businesstier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessTierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
